73ee89b76a2106a8ffa14966dd5aa467727a8c42
flamo939beast <23schua@live.elida.k12.oh.us>
2021-09-23 09:42:27 -0400

    part 2

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    public float xRange;
+    punlic float yRange;
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Keep player within x-range (left and right sides)
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Debug.Log(moveHorizontal);
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        transform.Translate(movement * speed * Time.deltaTime);
+
+       //if(Input.GetKeyDown(KeyCode.D))
+       // {
+       //     Debug.Log(Input.GetAxis("Horizontal"));
+       //     transform.Translate(Vector2.right * speed * Time.deltaTime);
+       // }
+    }
+}
