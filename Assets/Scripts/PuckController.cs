bbe481dad72553b3093b159f69a261a712f5fcf9
flamo939beast <23schua@live.elida.k12.oh.us>
2021-10-07 09:43:34 -0400

    Blocky added

---
+++ Assets/Scripts/PuckController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckController : MonoBehaviour
+{
+    public int[] direction = { 0, 1, 2, 3 };
+    public int moveDirection;
+    public float puckSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDirection = direction[Random.Range(0,4)];
+        Debug.Log("moveDirection = " + moveDirection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveDirection == 0)
+        {
+            transform.Translate(transform.up * puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 1)
+        {
+            transform.Translate(transform.up * -puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 2)
+        {
+            transform.Translate(transform.right * -puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 3)
+        {
+            transform.Translate(transform.right * puckSpeed * Time.deltaTime);
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Wall");
+        puckSpeed = -puckSpeed;
+    }
+
+}
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -7,20 +7,51 @@ public class Testing : MonoBehaviour
     public float xRange;
     public float yRange;
     public float speed = 10.0f;
+    public GameObject Puck;
+    public GameObject Blocky;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(Blocky, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+
+
+    }
+
+    private void LateUpdate()
+    {
+        //Keep player within x-range (left and right sides)
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
+        }
+
+        //Keep player within y-range (top and bottom sides)
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x, yRange);
+        }
+
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Keep player within x-range (left and right sides)
-        if(transform.position.x > xRange)
-        {
-            transform.position = new Vector2(xRange,transform.position.y);
-        }
+        //Instantiate(Puck,new Vector2(Random.Range(-xRange,xRange), Random.Range(-yRange, yRange)) , Quaternion.identity);
+
+        //Count how many enemies are in the scene
+        GameObject[] puckArray;
+        puckArray = GameObject.FindGameObjectsWithTag("Puck");
+
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         Debug.Log(moveHorizontal);
 
